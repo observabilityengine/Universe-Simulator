f29528b4863f153e069649278cd9f28c9e13950d
Ashley <308555169+observabilityengine@users.noreply.github.com>
2026-09-11 15:16:12 -0400

    Fix physics/__init__.py: remove missing two_body_1d import

--- physics/__init__.py
+++ physics/__init__.py
@@ -1,3 +1,3 @@
 from .nbody import NBodySystem, create_solar_system
-from .symbolic import SymbolicSystem, harmonic_oscillator, two_body_1d
+from .symbolic import SymbolicSystem, harmonic_oscillator
 from .fractal import mandelbrot, julia
