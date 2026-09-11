7bb1e1ace04d24d0be95675b6d685c36d56bc507
Ashley <308555169+observabilityengine@users.noreply.github.com>
2026-09-11 15:15:44 -0400

    Fix physics/__init__.py: remove import of non-existent cellular module

--- physics/__init__.py
+++ physics/__init__.py
@@ -1,4 +1,3 @@
 from .nbody import NBodySystem, create_solar_system
 from .symbolic import SymbolicSystem, harmonic_oscillator, two_body_1d
-from .cellular import GameOfLife, ElementaryCA
 from .fractal import mandelbrot, julia
